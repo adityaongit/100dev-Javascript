9828b6868e487379347f212d1bf79da8056f9ba1
Aditya Jindal <raghavjindal017@gmail.com>
2022-07-16 16:36:54 +0530

    q2 done

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -6,8 +6,14 @@ shows.forEach((show) => {
   console.log(show);
 });
 
-//Create and array of numbers
+//q2 Create and array of numbers
+let numbers = [1, 2, 5, 3, 8, 4, 6, 10, 7];
+
 //Return a new array of numbers that includes every even number from the previous Arrays
 
+let onlyEven = (arr) => arr.filter((n) => n % 2 === 0);
+
+
+console.log(onlyEven(numbers));
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
