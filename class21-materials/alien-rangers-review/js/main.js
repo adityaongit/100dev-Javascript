91047219070e96fd800eb56cab6a983e95575575
Aditya Jindal <raghavjindal017@gmail.com>
2022-07-16 16:25:32 +0530

    q1 done

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -1,6 +1,10 @@
 //Arrays
 
-//Create and array of tv shows. Loop through and print each show to the console
+//q1 Create and array of tv shows. Loop through and print each show to the console
+let shows = ['tmkoc', 'bbgph', 'fir'];
+shows.forEach((show) => {
+  console.log(show);
+});
 
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
