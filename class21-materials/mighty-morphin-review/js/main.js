7864ffa4b4724b54315cb5da9b17251d426fe94a
Aditya Jindal <raghavjindal017@gmail.com>
2022-07-16 14:42:25 +0530

    ques3 done

--- class21-materials/mighty-morphin-review/js/main.js
+++ class21-materials/mighty-morphin-review/js/main.js
@@ -1,21 +1,22 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
-var a = "diwali"
+var a = 'diwali';
 a = a.toUpperCase();
-console.log(a)
+console.log(a);
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
-let b = "aditya"
+let b = 'aditya';
 alert(b.slice(-3));
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
-
-
+function sub100(n1, n2, n3, n4, n5) {
+  return Math.abs(100 - (n1 + n2 + n3 + n4 + n5));
+}
+var res = sub100(5, 10, 15, 20, 25);
+console.log(res);
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
-
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
-
 
 //*Loops*
 //Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
